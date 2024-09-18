ad2c7eef6c8e23febd4bb7cd232e93febc40ce19
Son HN <Sonhn@hqgroups.vn>
2024-09-18 14:19:46 +0700

    CRUD table Trip

--- no1techwiz/routes/api.php
+++ no1techwiz/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TripController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,4 +24,12 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/trips', [TripController::class, 'index']);
+    Route::get('/trips/{id}', [TripController::class, 'show']);
+    Route::post('/trips', [TripController::class, 'store']);
+    Route::put('/trips/{id}', [TripController::class, 'update']);
+    Route::delete('/trips/{id}', [TripController::class, 'destroy']);
 });
