f39e354eccb6462f36bc2c590385acd7601b23f3
Son HN <Sonhn@hqgroups.vn>
2024-09-18 15:07:54 +0700

    fix bug

--- no1techwiz/routes/api.php
+++ no1techwiz/routes/api.php
@@ -30,25 +30,28 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
+Route::get('/trips', [TripController::class, 'index']);
+Route::get('/trips/{id}', [TripController::class, 'show']);
+
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/trips', [TripController::class, 'index']);
-    Route::get('/trips/{id}', [TripController::class, 'show']);
     Route::post('/trips', [TripController::class, 'store']);
     Route::put('/trips/{id}', [TripController::class, 'update']);
     Route::delete('/trips/{id}', [TripController::class, 'destroy']);
 });
 
+Route::get('/expenses', [ExpenseController::class, 'index']);
+Route::get('/expenses/{id}', [ExpenseController::class, 'show']);
+
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/expenses', [ExpenseController::class, 'index']);
-    Route::get('/expenses/{id}', [ExpenseController::class, 'show']);
     Route::post('/expenses', [ExpenseController::class, 'store']);
     Route::put('/expenses/{id}', [ExpenseController::class, 'update']);
     Route::delete('/expenses/{id}', [ExpenseController::class, 'destroy']);
 });
 
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
+
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/categories', [CategoryController::class, 'index']);
-    Route::get('/categories/{id}', [CategoryController::class, 'show']);
     Route::post('/categories', [CategoryController::class, 'store']);
     Route::put('/categories/{id}', [CategoryController::class, 'update']);
     Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
